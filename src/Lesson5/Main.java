3860b55707038ca8129b736856118d785b50f7cf
AzarovaLena <Azarova.l375@gmail.com>
2021-07-08 16:44:01 +0300

    задача создать компьютер

---
+++ src/Lesson5/Main.java
@@ -0,0 +1,13 @@
+package Lesson5;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Сomputer lena = new Сomputer();
+        lena.description();
+        lena.turnON();
+        lena.turnOFF();
+        lena.limit(9);
+
+    }
+}
